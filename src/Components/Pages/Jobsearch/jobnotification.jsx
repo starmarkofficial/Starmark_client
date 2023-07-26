e2d299a237ce46e8b0ae0d9e07fbba1dcc0dc20c
Subham Sinha <gitsubham21@gmail.com>
2023-07-26 20:13:05 +0530

    Add Job Card

--- src/Components/Pages/Jobsearch/jobnotification.jsx
+++ src/Components/Pages/Jobsearch/jobnotification.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from '../../Home/Nav'
 import Searchbar from './searchbar';
+import JobList from './JobList'
 export default function jobnotification() {
   return (
     <div>
       <Nav/>
      <Searchbar/>
+     <JobList />
     </div>
   )
 }
